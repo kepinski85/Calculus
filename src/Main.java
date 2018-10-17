1ecde65c22314d92ebe7be42d306f1380895b7b0
Kempa <kepinski85@gmail.com>
2018-10-17 20:43:16 +0200

    cosmetics

--- src/Main.java
+++ src/Main.java
@@ -6,16 +6,9 @@ public class Main {
     public static void main(String[] args) {
 
         Calculator calc = new Calculator();
+
         System.out.println("Enter numbers and signs with space between: ");
 
-
-            calc.calculator();
-
-
-
-
-
-
-
+        calc.calculator();
     }
 }
